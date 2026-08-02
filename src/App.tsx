b050b7289c926d265b98e5c5b5a0560604f19ae7
Rostislove <rostislove.rh14@gmail.com>
2026-08-02 13:48:20 +0300

    Upd Project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { TextGenerateEffect } from './components/ui/text-generate-effect.tsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <TextGenerateEffect words={'I\'m Rostislav Tyan, a product designer with nearly a decade of experience. I love solving complex problems through thoughtful design, collaborating across disciplines, and staying curious through both my work and my travels.'}/>
+      </section>
+    </>
+  )
+}
+
+export default App
